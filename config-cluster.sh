b7210d5daea7bd2d5a04f6f941bdaecacfdb1ea0
gesse <gessedafe@gmail.com>
2018-09-17 15:44:48 -0300

    logbeat and kibana talking to each other

--- config-cluster.sh
+++ config-cluster.sh
@@ -1,57 +1,73 @@
 # ======================================
-# ENV VARS
+# Defines a path for the cluster startup
+# custom script
 # ======================================
 DIND_CLUSTER_STARTUP_FILE=./vendor_kubeadm-dind-cluster/fixed/dind-cluster-v1.11-gessedafe.sh
 
 
 # ======================================
-# Building ELK Docker custom image
-# ======================================
-docker build ./configs/elk-docker-image-no-ssl --tag gessedafe/elk:1.0.0
-
-
-# ======================================
-# Cloning and changing the startup file to
-# activate the audit logging feature
+# Clones and changes the cluster startup
+# file to activate the audit logging
+# feature
 # ======================================
 cp ./vendor_kubeadm-dind-cluster/fixed/dind-cluster-v1.11.sh $DIND_CLUSTER_STARTUP_FILE
 sed -i  '' 's/{CoreDNS: false}/{CoreDNS: false, Auditing: true}/g' $DIND_CLUSTER_STARTUP_FILE
 
 
 # ======================================
-# Starting the cluster
+# Starts the cluster
 # ======================================
 $DIND_CLUSTER_STARTUP_FILE up
 
 
 # ======================================
-# Discovering master node's container
+# Discovers the name of master node's
+# container
 # ======================================
 DIND_CLUSTER_MASTER_CONTAINER=`docker ps | grep master | awk '{print $1}'`
 
 
 # ======================================
-# Copying filebeat config to master
+# Copies filebeat config to master
 # container
 # ======================================
-docker cp ./filebeat-docker/filebeat.yml $DIND_CLUSTER_MASTER_CONTAINER:/usr/share/filebeat/filebeat.yml
+docker exec $DIND_CLUSTER_MASTER_CONTAINER mkdir /usr/share/filebeat
+docker cp ./filebeat/filebeat.yml $DIND_CLUSTER_MASTER_CONTAINER:/usr/share/filebeat/filebeat.yml
 
 
 # ======================================
-# Deploying ELK
+# Builds the ELK Docker custom image
 # ======================================
-kubectl apply -f ./configs/elk.yml
+docker build ./configs/elk-docker-image-no-ssl --tag gessedafe/elk:1.0.0.1
 
 
 # ======================================
-# Deploying Filebeat
+# Pushes the ELK image into Docker Hub
 # ======================================
-kubectl apply -f ./configs/filebeat-deamonset.yaml
+docker push gessedafe/elk:1.0.0.1
 
 
 # ======================================
-# Forwarding cluster ports
+# Deploys ELK
 # ======================================
-./configs/elk-port-forwarding.sh
+kubectl apply -f ./elk/elk-deploy-service.yml
+
+
+# ======================================
+# Creates a config map for filebeat
+# ======================================
+kubectl create configmap filebeat-config --from-file ./filebeat/filebeat.yml -o yaml --dry-run | kubectl apply -f -
+
+
+# ======================================
+# Deploys Filebeat
+# ======================================
+kubectl apply -f ./filebeat/filebeat-daemonset.yml
+
+
+# ======================================
+# Forwards cluster ports
+# ======================================
+./elk/elk-port-forwarding.sh
 
 
--- filebeat/docker-run-filebeat.sh
+++
@@ -1,2 +0,0 @@
-docker run --user root --mount type=bind,source="/usr/share/filebeat/filebeat.yml,target=/usr/share/filebeat/filebeat.yml" elastic/filebeat:6.4.0
-
