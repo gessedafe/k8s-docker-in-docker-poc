6a17803575a21c3e9f9cc3ea5df6658739773463
gesse <gessedafe@gmail.com>
2018-09-17 17:58:15 -0300

    sendind audit policy files to master node

---
+++ 02-run.sh
@@ -0,0 +1,6 @@
+# ======================================
+# Starts the cluster
+# ======================================
+DIND_CLUSTER_STARTUP_FILE=./vendor_kubeadm-dind-cluster/fixed/dind-cluster-v1.11-gessedafe.sh
+
+$DIND_CLUSTER_STARTUP_FILE up
--- 03-deploy.sh
+++ 03-deploy.sh
@@ -1,26 +1,4 @@
 # ======================================
-# Defines a path for the cluster startup
-# custom script
-# ======================================
-DIND_CLUSTER_STARTUP_FILE=./vendor_kubeadm-dind-cluster/fixed/dind-cluster-v1.11-gessedafe.sh
-
-
-# ======================================
-# Clones and changes the cluster startup
-# file to activate the audit logging
-# feature
-# ======================================
-cp ./vendor_kubeadm-dind-cluster/fixed/dind-cluster-v1.11.sh $DIND_CLUSTER_STARTUP_FILE
-sed -i  '' 's/{CoreDNS: false}/{CoreDNS: false, Auditing: true}/g' $DIND_CLUSTER_STARTUP_FILE
-
-
-# ======================================
-# Starts the cluster
-# ======================================
-$DIND_CLUSTER_STARTUP_FILE up
-
-
-# ======================================
 # Discovers the name of master node's
 # container
 # ======================================
@@ -63,11 +41,3 @@ kubectl create configmap filebeat-config --from-file ./filebeat/filebeat.yml -o 
 # Deploys Filebeat
 # ======================================
 kubectl apply -f ./filebeat/filebeat-daemonset.yml
-
-
-# ======================================
-# Forwards cluster ports
-# ======================================
-./elk/elk-port-forwarding.sh
-
-
