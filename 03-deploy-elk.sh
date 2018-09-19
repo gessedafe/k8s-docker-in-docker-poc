113904c0b5c3d638019dcd197ec8a9eda55e56cb
gesse <gessedafe@gmail.com>
2018-09-19 12:02:10 -0300

    Configuring logstash filter to use audit specific index

--- 03-deploy-elk.sh
+++ 03-deploy-elk.sh
@@ -1,13 +1,13 @@
 # ======================================
 # Builds the ELK Docker custom image
 # ======================================
-docker build ./configs/elk-docker-image-no-ssl --tag gessedafe/elk:1.0.0.7
+docker build ./configs/elk-docker-image-no-ssl --tag gessedafe/elk:1.0.0.8
 
 
 # ======================================
 # Pushes the ELK image into Docker Hub
 # ======================================
-docker push gessedafe/elk:1.0.0.7
+docker push gessedafe/elk:1.0.0.8
 
 
 # ======================================
